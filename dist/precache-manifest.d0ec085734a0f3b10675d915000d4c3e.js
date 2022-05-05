ff5fd4ce1632c40d9c53323035408993eab3a90a
AsbertMa <stayinalive@msn.cn>
2022-05-05 11:14:35 +0800

    git pages

---
+++ dist/precache-manifest.d0ec085734a0f3b10675d915000d4c3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9dfea5dee6891757068",
+    "url": "/css/app.1a9ebaad.css"
+  },
+  {
+    "revision": "089d9a5f12b9f4d198aeda08e7b49876",
+    "url": "/img/ethereum.089d9a5f.png"
+  },
+  {
+    "revision": "4a2f1aea8f0af33da64e8623ae8a2eee",
+    "url": "/img/vechain.4a2f1aea.png"
+  },
+  {
+    "revision": "54f924e7551408f2ad546c9928f10dd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9dfea5dee6891757068",
+    "url": "/js/app.2847f9ed.js"
+  },
+  {
+    "revision": "05233e2811654d079b02",
+    "url": "/js/chunk-vendors.4a908b59.js"
+  },
+  {
+    "revision": "d30015e030856156c93d592a2d2ee93b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
